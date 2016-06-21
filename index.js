9843d6ea8cfa15e9fe1c6091c47af66d61f9249d
Carlos Azaustre <cazaustre@gmail.com>
2016-06-21 22:04:16 +0200

    add principal function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 'use strict'
+
+/**
+ * Generate a URL with spaces replaced by underscores (SEO Friendly).
+ * @param {string} name - Text to transform in a URL.
+ * @return {string} The URL generated.
+ */
+function generateURL (name) {
+  return name
+    .toLowerCase()
+    .split('')
+    .map(letter => (letter === ' ') ? '-' : letter)
+    .join('')
+}
+
+module.exports = generateURL
